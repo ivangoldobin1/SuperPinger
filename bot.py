13887c368c5bf5357495a14ee6192f592e48fa74
Ivan Goldobin <68515967+ivangoldobin1@users.noreply.github.com>
2023-06-15 12:28:17 +0500

    Update

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import os
 import urllib.request
-from setting import API_TOKEN, PING_TIME, IP_LIST, DOMAIN_LIST, CHAT_IDS
+from typing import List, Any
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types
+from setting import API_TOKEN, PING_TIME, IP_LIST, DOMAIN_LIST, CHAT_IDS
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -11,39 +12,39 @@ menu_buttons = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 menu_buttons.row(types.KeyboardButton(text="🔬 Тестирование локальной сети"), types.KeyboardButton(text="🔄 Автоматическое тестирование"))
 
 async def get_ping_result(node):
-    response = os.system("ping -c 1 " + node)
-    return '\u2705' if response == 0 else '\U0000274c'
+    response = os.system("ping -c 1 " + node[0])
+    return '✅' if response == 0 else '❌'
 
 async def get_http_response_code(node):
     try:
-        response = urllib.request.urlopen(node).getcode()
+        response = urllib.request.urlopen(node[0]).getcode()
     except:
         response = 404
     return response
 
 async def send_network_test_results_error():
-    error_message = "Следующие серверы недоступны:"
+    error_message = 'Следующие серверы недоступны:\n'
     error_occurred = False
 
     for node in IP_LIST:
         result = await get_ping_result(node)
-        if result == '\U0000274c':
+        if result == '❌':
             error_occurred = True
-            error_message += f"\n{result} {node}"
+            error_message += f"\n❌ {node[1]}\nIP адрес {node[0]}\n"
 
     for node in DOMAIN_LIST:
-        response = await get_http_response_code(node[0])
+        response = await get_http_response_code(node)
         if response != 200:
             error_occurred = True
-            error_message += f"\n{result} {node[1]} ({response})"
-    
+            error_message += f"\n❌ {node[1]}\nДомен {node[0]}({response})\n"
+
     if error_occurred:
         for chat_id in CHAT_IDS:
             await bot.send_message(chat_id=chat_id, text=error_message, reply_markup=menu_buttons)
 
 @dp.message_handler(text='🔄 Автоматическое тестирование')
 async def send_welcome(message: types.Message):
-    msg = "Запущенно автоматическое тестирование локальной сети...\nДля остановки тестирования остановите бота на сервере"
+    msg = "Запущено автоматическое тестирование локальной сети...\nДля остановки тестирования остановите бота на сервере"
     await message.answer(msg, reply_markup=menu_buttons)
     while True:
         await send_network_test_results_error()
@@ -51,18 +52,18 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler(text=['🔬 Тестирование локальной сети'])
 async def send_welcome(message: types.Message):
-    msg = "Запущенно тестирование локальной сети..."
+    msg = "Запущено тестирование локальной сети..."
     await message.answer(msg, reply_markup=menu_buttons)
-    answer = "Результат тестирования локальной сети:"
+    answer = "Результат тестирования локальной сети:\n"
     for node in IP_LIST:
         result = await get_ping_result(node)
-        answer += f"\n{result} {node}"
-    
+        answer += f"\n{result} {node[1]}\nIP адрес {node[0]}\n"
+
     for node in DOMAIN_LIST:
-        response = await get_http_response_code(node[0])
-        result = '\u2705' if response == 200 else '\U0000274c'
-        answer += f"\n{result} {node[1]} ({response})"
-    
+        response = await get_http_response_code(node)
+        result = '✅' if response == 200 else '❌'
+        answer += f"\n{result} {node[1]}\nДомен {node[0]} ({response})\n"
+
     await message.reply(answer, reply_markup=menu_buttons)
 
 @dp.message_handler(commands=['start'])
--- setting.py
+++ setting.py
@@ -2,21 +2,23 @@ from typing import List, Any
 
 # Лист тестируемых ip адресов
 IP_LIST = [
-    "8.8.8.8",
-    "8.8.4.4"
+    ["8.8.8.8", "DNS Google"],
+    ["192.192.192.192", "Null IP"]
 ]
+
 # Лист тестируемых доменов
 DOMAIN_LIST: List[Any] = [
-    ["https://google.ru", "Google.ru"],
-    ["https://ya.ru", "Ya.ru"]
+    ["https://google.ru", "Гугл"],
+    ["https://yaрпм32наигуп.ru", "Яндекс"]
 ]
 
 # https://api.telegram.org/botAPI_TOKEN/getUpdates
 CHAT_IDS = [
+    ""
 ]
 
 # В кавычки вставить API для бота полученный от @BotFather
 API_TOKEN = '' 
 
 # Время автоматического тестирования в секундах
-PING_TIME = 300
+PING_TIME = 30
